df10bb7f21d36aecdfee25b6a9b56e47f6ea2db4
Tales Noronha <talesnoronha@gmail.com>
2024-10-26 11:55:52 -0300

    Aula 05

---
+++ Aula 05 - Introducao Python/arquivo01.py
@@ -0,0 +1,24 @@
+#print("Hello World")
+#print("Olá Mundo")
+#print("Hello World!")
+
+nome = "João"
+idade = 20
+altura = 1.70
+
+#print(nome, "possui" , idade , "anos e " , altura ,"de altura.")
+
+# #Template String
+# print("%s, tem %s anos, e possui %s de altura." %(nome, idade, altura))
+# print('{}, tem {} anos, e possui {} de altura.' .format (nome, idade, altura))
+# print(f'{nome}, tem {idade} anos, e possui {altura} de altura.')
+
+
+#-----------ENTRADA E SAÍDA DE DADOS-----------------
+#print -> saídad de dados
+novo_nome = input("Digite um nome.")
+nova_idade = int(input("Digite uma idade."))
+nova_altura = float(input("Digite uma altura."))
+print(f'{novo_nome}, tem {nova_idade} anos, e possui {nova_altura} de altura.')
+
+print(type(7/2))
