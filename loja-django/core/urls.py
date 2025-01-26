265926473368157f5f229cd154a4c3f4ecaea85f
Taheno <talesnoronha@gmail.com>
2025-01-26 13:56:54 -0300

    Imagens ajustadas

--- loja-django/core/urls.py
+++ loja-django/core/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
 from app.views import data_atual
 from app.views import ProductListView, ProductCreateView, ProductDetailView, ProductUpdateView, ProductDelete
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,5 +13,6 @@ urlpatterns = [
     path('create/', ProductCreateView.as_view(), name='product-create' ),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product-detail' ),
     path('product/<int:pk>/update', ProductUpdateView.as_view(), name='product-update' ),
-    path('product/delete/<int:pk>', ProductDelete.as_view(), name='product-delete' )
-]
+    path('product/delete/<int:pk>', ProductDelete.as_view(), name='product-delete' ),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
